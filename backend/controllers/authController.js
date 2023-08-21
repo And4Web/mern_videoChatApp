087032c49e9e9ac54ef745f68db9224c87c0352e
unknown <anands2807@gmail.com>
2023-08-21 12:11:00 +0530

    proper folder structure and MVC pattern implemented

---
+++ backend/controllers/authController.js
@@ -0,0 +1,7 @@
+exports.login = async (req, res) => {
+  res.send("Login endpoint......")
+}
+
+exports.register = async (req, res) => {
+  res.send("Register endpoint.....")
+}
--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,25 @@ require('dotenv').config();
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// import Routes
+const authRoutes = require('./routers/authRoutes');
+
+// implement Routes
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res)=>{
+  return res.status(200).json({"message": "Hello world!"})
+})
+
+// Create server
 const server = http.createServer(app);
 
+// Connect Database
 mongoose.connect(process.env.MONGO_URI).then(()=>{
   server.listen(PORT, ()=>{
     console.log("~-MongoDB connection successful.");
