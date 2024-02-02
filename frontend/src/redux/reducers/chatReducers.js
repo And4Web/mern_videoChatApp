e4e1722638a09030a8a01d2588b0a3b1fc939f82
Ananad Shukla <anands2807@gmail.com>
2024-02-02 12:20:19 +0530

    chatAction.js and chatReducer.js created to manage chat functionality and states in redux store

---
+++ frontend/src/redux/reducers/chatReducers.js
@@ -0,0 +1,33 @@
+import { chatActions } from "../actions/chatActions"
+
+const initialState = {
+  chosenChatDetails: null,
+  chatTypes: null,
+  messages: []
+}
+
+const chatReducer = (state=initialState, action) => {
+  switch(action.type){
+    case chatActions.SET_CHOSEN_CHAT_DETAILS:
+      return {
+        ...state,
+        chosenChatDetails: action.chatDetails,
+        chatTyes: action.chatType,
+        messages: []
+      }
+    case chatActions.SET_MESSAGES:
+      return {
+        ...state,
+        messages: action.messages
+      }
+    case chatActions.SET_CHAT_TYPE:
+      return {
+        ...state,
+        chatType: action.chatType
+      }
+    default: return state
+  }
+}
+
+
+export default chatReducer
