91e564b632395cfbf98b15b3fc3ae0f97bb6ba77
Ananad Shukla <anands2807@gmail.com>
2024-02-27 11:29:09 +0530

    implemented mic off feature in MicButton.js component by using localStream.getAudioTracks()[0].enabled

--- frontend/src/shared/components/MicButton.js
+++ frontend/src/shared/components/MicButton.js
@@ -3,10 +3,11 @@ import IconButton from '@mui/material/IconButton';
 import Mic from '@mui/icons-material/Mic'
 import MicOff from '@mui/icons-material/MicOff'
 
-function MicButton() {
+function MicButton({localStream}) {
   const [micEnabled, setMicEnabled] = useState(true);
 
   const handleMicToggle = () => {
+    localStream.getAudioTracks()[0].enabled = !micEnabled;
     setMicEnabled(!micEnabled);
   }
   return (
